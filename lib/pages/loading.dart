7b04b673a368531373bf9195eda3256328e4cd0a
Niraj Pant <nirajbpant@gmail.com>
2020-11-02 16:33:34 +0545

    Final commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+
+  void setUpWorldTime() async{
+    WorldTime worldTime= WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await worldTime.getTime();
+    Navigator.pushReplacementNamed(context, '/home',arguments: {
+      'location': worldTime.location,
+      'flag': worldTime.flag,
+      'time': worldTime.time,
+      'isDaytime' : worldTime.isDayTime,
+    });
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    setUpWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitRing(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
